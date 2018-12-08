332dda6698f5a00cefb357df51a117282f74a471
RobertYL <rob3rtyl33@gmail.com>
2018-12-07 18:27:30 -0800

    Added radius calculations into refactored code, TODO: position, velocity, acceleration calculations

--- src/PathGeneration.py
+++ src/PathGeneration.py
@@ -1,3 +1,4 @@
+import copy
 import math
 
 '''
@@ -9,11 +10,11 @@ Velocities
 '''
 
 class PathGeneration(object):
-    base = []
-    injected = []
-    smoothed = []
-    pathC = []
-    pathV = []
+    base = None
+    injected = None
+    smoothed = None
+    pathC = None
+    pathV = None
 
     def __init__(self, base):
         if(len(base) < 2):
@@ -27,8 +28,10 @@ class PathGeneration(object):
         else:
             return math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2))
 
-
     def injectPath(self, spacing):
+        if(self.base is None):
+            print("Path not created")
+            return
         x = []
         y = []
         for node in range(len(self.base[0][:-1])):
@@ -39,3 +42,44 @@ class PathGeneration(object):
                 x.append(self.base[0][node] + ((int)(deltaX * iter / toInject)))
                 y.append(self.base[1][node] + ((int)(deltaY * iter / toInject)))
         self.injected = [x, y]
+
+    def smoothPath(self, a, b, tolerance):
+        if(self.injected is None):
+            print("Path not injected")
+            return
+        self.smoothed = copy.deepcopy(self.injected)
+        change = tolerance
+        while(change >= tolerance):
+            change = 0.0
+            for i in range(1, len(self.smoothed[0])-1):
+                for j in range(0, 2):
+                    add = a * (self.injected[j][i] - self.smoothed[j][i])
+                    add2 = b * (self.smoothed[j][i-1] + self.smoothed[j][i+1] - (2.0 * self.smoothed[j][i]))
+
+                    aux = self.smoothed[j][i]
+                    self.smoothed[j][i] += add
+                    self.smoothed[j][i] += add2
+                    change += math.fabs(aux - self.smoothed[j][i])
+
+    def radiusPath(self):
+        if(self.smoothed is None):
+            print("Path not smoothed")
+            return
+        self.pathC = []
+        self.pathC.append(0)
+        for node in range(1, len(self.smoothed[0][:-1])):
+            x1 = self.smoothed[0][node] + 0.00001
+            y1 = self.smoothed[1][node]
+            x2 = self.smoothed[0][node - 1]
+            y2 = self.smoothed[1][node - 1]
+            x3 = self.smoothed[0][node + 1]
+            y3 = self.smoothed[1][node + 1]
+
+            c1 = (x1*x1 + y1*y1 - x2*x2 - y2*y2)/(2*(x1 - x2))
+            c2 = (y1-y2)/(x1-x2)
+            b = (x2*x2 - 2*x2*c1 + y2*y2 - x3*x3 + 2*x3*c1 - y3*y3)/(2*(x3*c2 - y3 + y2 - x2*c2))
+            a = c1 - c2*b
+            r = math.sqrt((x1-a)**2 + (y1-b)**2)
+
+            self.pathC.append(r)
+        self.pathC.append(0)
--- src/main.py
+++ src/main.py
@@ -3,15 +3,19 @@ import matplotlib.pyplot as plt
 from PathGeneration import PathGeneration
 
 
+debug = True
+
 basePath = [[50, 100, 160, 190], [20, -50, 20, -40]]
 spacing = 15
 
 pathGen = PathGeneration(basePath)
 pathGen.injectPath(spacing)
+pathGen.smoothPath(0.6, 0.4, 0.01)
+pathGen.radiusPath()
 
 fig, ax = plt.subplots()
 plt.xlim(0, 300)
 plt.ylim(-100, 200)
-ax.scatter(pathGen.injected[0], pathGen.injected[1])
+ax.scatter(pathGen.smoothed[0], pathGen.smoothed[1])
 plt.show()
 #pathFol = PathFollowing()
